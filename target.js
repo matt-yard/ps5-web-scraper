b212653b232bf78ac3ebf88f926b7054946c0b90
Matt Yard <mattyard11@gmail.com>
2022-10-07 15:33:03 -0400

    adds functions to fetch target API (work in progress)

--- target.js
+++ target.js
@@ -3,20 +3,62 @@ const chalk = require("chalk");
 require("dotenv").config({ path: "./.env" });
 const { fetchHTML, parseHTML } = require("./utilities");
 
-// Target's stock and add to cart button are added with a JS script,
-// so they dont show up on the HTML file that loads with the get request,
-// going to attept to use their API directly to access stock information
+// This function makes a request to target.com, then parses the request
+// cookies for a visitorId
+// the visitorId can be used as a key to access the target API
+// const getVisitorId = async () => {
+//   const result = await axios.get("https://www.target.com");
+
+//   let cookie = result.headers["set-cookie"];
+//   const visitorIdIndex = cookie.indexOf("visitorId=");
+
+//   let visitorId = cookie.slice(visitorIdIndex);
+//   visitorId = visitorId.slice(
+//     visitorId.indexOf("=") + 1,
+//     visitorId.indexOf(";")
+//   );
+//   console.log(visitorId);
+//   return visitorId;
+// };
+
+// const fetchAndParseTarget = async (key, productId, locationId) => {
+//   try {
+//     const url = `https://redsky.target.com/redsky_aggregations/v1/web_platform/product_fulfillment_v1`;
+
+//     const { data } = await axios.get(url, {
+//       params: {
+//         key,
+//         scheduled_delivery_store_id: locationId,
+//         required_store_id: locationId,
+//         has_required_store_id: true,
+//         tcin: productId,
+//         is_bot: false,
+//       },
+//     });
+
+//     console.log(data.data);
+//     return data;
+//   } catch (error) {
+//     console.log("Error in parsing....", error);
+//     return false;
+//   }
+// };
+
+// const checkStockTarget = async () => {
+//   const visitorId = await getVisitorId();
+//   const result = await fetchAndParseTarget(visitorId, "81114595", 1348);
+// };
+
 const fetchAndParseTarget = async (url) => {
   const result = await fetchHTML(url);
-
-  console.log(result);
-  try {
-  } catch (error) {
-    console.log("Error in parsing....", error);
-    return false;
-  }
+  let body = parseHTML(result).getElementsByTagName("body")[0];
+  let button = body.getElementById("addToCartButtonOrTextIdFor81114595");
+  console.log(button);
 };
 
-const checkStockTarget = async () => {};
+fetchAndParseTarget(
+  "https://www.target.com/p/playstation-5-console/-/A-81114595"
+);
 
-fetchAndParseTarget();
+// const checkStockTarget = () => {};
+// checkStockTarget();
