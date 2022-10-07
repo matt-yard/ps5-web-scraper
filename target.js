14132dcc287b544c198096bdec8e38bd2c71da93
Matt Yard <mattyard11@gmail.com>
2022-10-07 01:15:38 -0400

    adds more comments

--- target.js
+++ target.js
@@ -3,6 +3,9 @@ const chalk = require("chalk");
 require("dotenv").config({ path: "./.env" });
 const { fetchHTML, parseHTML } = require("./utilities");
 
+// Target's stock and add to cart button are added with a JS script,
+// so they dont show up on the HTML file that loads with the get request,
+// going to attept to use their API directly to access stock information
 const fetchAndParseTarget = async (url) => {
   const result = await fetchHTML(url);
 
